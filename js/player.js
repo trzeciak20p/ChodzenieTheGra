a1c285fc947d23698619be652e8d610ff7ff0fff
Marekse <m.trzeciak.p20@gmail.com>
2022-09-09 20:21:04 +0200

    jump, duck ignition by key presses

--- js/player.js
+++ js/player.js
@@ -1,8 +1,24 @@
 
 
-document.addEventListener("keydown", function (event) {         
-    if (event.code == 32) {          
+document.addEventListener("keydown", function (event) {              
+    if (event.code == "KeyW" || event.code == "Space" || event.code == "ArrowUp") {   //wykrywanie w, s, strzałek, spacji i przypisanie im skoku, kucnięcia          
         event.preventDefault()
+        Jump()
+    }else if(event.code == "KeyS" || event.code == "ArrowDown"){
+        event.preventDefault()
+        Duck()
     }
-
 })
+
+
+
+function Jump(){
+    console.log("jump")
+
+}
+
+function Duck(){
+    console.log("duck")
+
+
+}
