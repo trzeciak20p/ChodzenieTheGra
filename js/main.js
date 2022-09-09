35e1358289526620be971293d627813c0c23d3d7
Marekse <m.trzeciak.p20@gmail.com>
2022-09-09 20:10:59 +0200

    folder reorganization

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const options = document.getElementById("options")
+const body = document.getElementsByTagName("body")
+
+const canvas = document.createElement("canvas")
+document.getElementsByTagName("main")
+
+
+document.addEventListener("resize", ZmianaRozmiaru)
+
+// 
+
+
+
+
+function ZmianaRozmiaru(){
+    c.height = body.height - 90     // wywalamy h nava   
+    c.width = body.width
+}
+
+
+function Skok(){
+
+
+}
+
+function Kuc(){
+
+
+}
