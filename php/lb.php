acd69cea551e9025c8a7bbf2355a5b7642525156
Marekse <m.trzeciak.p20@gmail.com>
2022-09-19 22:04:07 +0200

    lb.php

---
+++ php/lb.php
@@ -0,0 +1,14 @@
+<?php
+
+ob_start();
+
+// if( isset($_SESSION["login"]) ){
+//     echo "<a href='login.php'>Witaj <span style='text-transform: capitalize;'>" . $_SESSION["login"]. "</span></a>";
+// }else{
+//     echo "<a href='login.php'>Zaloguj się</a>";
+// }
+
+
+
+
+
