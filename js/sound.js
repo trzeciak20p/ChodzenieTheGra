8387882889b1fb5a8085d6f7bb3535e0714b980a
Marekse <m.trzeciak.p20@gmail.com>
2022-10-10 20:35:57 +0200

    kick loop I hope

--- js/sound.js
+++ js/sound.js
@@ -5,17 +5,33 @@ document.addEventListener("keydown", function (event) {     //chwilowy input do 
     }
 })
 
-function Hitsound(){
+
+const clock = new Tone.Clock(time => {
+    if(Game.game_state == true){
+        console.log(time)
+    }  
+}, 1);
+
+const kicks = new Tone.Player({
+    url: "../sound/kick.mp3",
+    autostart: true,
+});
+
+class SoundClass{
 
 
+    constructor(){}
+
+    GameStart(){
+        kicks.startLoop()
+        clock.start();
+
+    }
 
 
 }
 
+let Sound = new SoundClass()
 
-const clock = new Tone.Clock(time => {
-	console.log(time);
-}, 1);
 
-clock.start();
 
