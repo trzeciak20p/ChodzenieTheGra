e6e94ea0551f977482d918c135837192a7bb5b79
Marekse <m.trzeciak.p20@gmail.com>
2022-10-10 23:20:38 +0200

    blue box

--- js/canvas.js
+++ js/canvas.js
@@ -2,6 +2,11 @@
 
 //zamiecić potem na realną grafike
 
+let blue_box = new Image()
+blue_box.src = "graphics/objects/blue_box.png";
+
+
+
 class Objectile{
 
     constructor(property, speed = 8){
@@ -9,7 +14,7 @@ class Objectile{
         this.pos_x = Game.window_w
         this.property        //nadciągający przeciwnicy 0 - brak,  1 - dół-unik, 2 - dół-atak, 3 - góra-unik, 4 - góra-atak
         this.speed = speed
-        this.size = 30       //do wywalenia
+        this.size = 100       //do wywalenia
         this.image
 
         if(property <= 2){
@@ -25,9 +30,12 @@ class Objectile{
     }
 
     DrawObjectile(){
-        ctx.fillStyle = "rgb(220,12,12)"
-        ctx.beginPath()
-        ctx.fillRect(this.pos_x, this.pos_y, this.size, this.size)        
+        ctx.drawImage(blue_box, this.pos_x, this.pos_y, this.size, this.size) 
+
+
+        // ctx.fillStyle = "rgb(220,12,12)"
+        // ctx.beginPath()
+        // ctx.fillRect(this.pos_x, this.pos_y, this.size, this.size)        
     }
 
 }
