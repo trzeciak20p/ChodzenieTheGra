334f1aa66b4d3a36e8a319af0d4c76c629f84721
Marekse <m.trzeciak.p20@gmail.com>
2022-10-11 21:54:33 +0200

    spawnowanie z poprawną grafiką

--- js/canvas.js
+++ js/canvas.js
@@ -1,26 +1,28 @@
 
-
-//zamiecić potem na realną grafike
-
-let blue_box = new Image()
-blue_box.src = "graphics/objects/blue_box.png";
-
-
-
 class Objectile{
 
     constructor(property, speed = 8){
         this.pos_y
         this.pos_x = Game.window_w
-        this.property        //nadciągający przeciwnicy 0 - brak,  1 - dół-unik, 2 - dół-atak, 3 - góra-unik, 4 - góra-atak
+        this.property = property        //nadciągający przeciwnicy 0 - brak, 1 - góra-atak, 2 - góra-unik, 3 - dół-atak, 4 - dół-unik,
         this.speed = speed
         this.size = 100       //do wywalenia
         this.image
 
-        if(property <= 2){
-            this.pos_y = Math.round(Game.window_h / 3 * 2)
+        this.blue_box = new Image()
+        this.blue_box.src = "graphics/objects/blue_box.png";
+        this.pink_box = new Image()
+        this.pink_box.src = "graphics/objects/pink_box.png";
+        this.blue_bomb = new Image()
+        this.blue_bomb.src = "graphics/objects/blue_bomb.png";
+        this.pink_bomb = new Image()
+        this.pink_bomb.src = "graphics/objects/pink_bomb.png";
+        this.graphic = [this.blue_box, this.pink_bomb, this.pink_box, this.blue_bomb]
+
+        if(this.property == 1 || this.property == 2){
+            this.pos_y = Math.round(Game.window_h / 3)      //góra
         }else{
-            this.pos_y = Math.round(Game.window_h / 3)
+            this.pos_y = Math.round(Game.window_h / 3 * 2)  //dół
         }
     }
 
@@ -30,7 +32,7 @@ class Objectile{
     }
 
     DrawObjectile(){
-        ctx.drawImage(blue_box, this.pos_x, this.pos_y, this.size, this.size) 
+        ctx.drawImage(this.graphic[this.property-1], this.pos_x, this.pos_y, this.size, this.size) 
 
 
         // ctx.fillStyle = "rgb(220,12,12)"
