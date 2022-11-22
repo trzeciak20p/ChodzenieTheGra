8208d92bfc7f539175e29f261f41de6be40eb20a
trzeciak20p <91263438+trzeciak20p@users.noreply.github.com>
2022-11-22 11:26:06 +0100

    WIP creating selector

--- js/keybinds.js
+++ js/keybinds.js
@@ -28,8 +28,33 @@ class KeybindsClass{
         tab.push(input)
     }
 
+    CreateKeybindsAdjustments(where){
 
+        this.selector = document.createElement("DIV")
+        this.selector.setAttribute("class", "keybinds_selector")
+        this.CreateSelector(this.binds_duck)
 
+        where.appendChild(this.selector)
+
+    }
+
+    CreateSelector(tab){
+
+        let selector = document.createElement("select")
+
+        for(let i in tab){
+            // this.dictionary[this.data[String(i)]["name"]] = i       //słownik do wczytywania numerka motywu po nazwie
+            // this.option = document.createElement("option")
+            // this.option.innerText = i.key
+            key =  new KeyboardEvent("keydown", , tab[i])
+
+            console.log(tab[i],key.description)
+            // selector.addEventListener("input", () => { this.ChangeTheme(this.dictionary[this.selector.value]) })       //zmiana motywu przy wybraniu
+            // selector.appendChild(this.option)
+        }
+
+        return selector;
+    }
 
 }
 
