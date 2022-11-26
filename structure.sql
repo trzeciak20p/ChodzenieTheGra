96d2bc04fc820b9cb7a5c60ebb7201c5ed9c768a
Marekse <m.trzeciak.p20@gmail.com>
2022-11-26 14:17:51 +0100

    i dont wanna talk bout this

--- structure.sql
+++ structure.sql
@@ -1,7 +1,8 @@
-use ChodzenieTheGame
-create table Users{
-    id int auto_increment not null,
+use ChodzenieTheGra
+
+create table Users(
+    id int auto_increment primary key not null,
     username varchar(30) not null,
     password varchar(30) not null,
     best_score int
-};
+);
