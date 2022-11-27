8e46eaf8effdae65026af5aa99382d8d2245e93a
Marekse <m.trzeciak.p20@gmail.com>
2022-11-27 19:21:46 +0100

    final product

--- structure.sql
+++ structure.sql
@@ -1,7 +1,7 @@
-use ChodzenieTheGra
+use ChodzenieTheGra;
 
 create table Users(
-    id int auto_increment primary key not null,
+    id int auto_increment primary key,
     username varchar(30) not null,
     password varchar(30) not null,
     best_score int
