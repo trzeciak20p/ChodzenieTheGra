9620b1c83a9b662a7daa7e5fdb195a516aabb369
Marekse <m.trzeciak.p20@gmail.com>
2022-11-27 19:19:23 +0100

    logowanie działa

--- php/login.php
+++ php/login.php
@@ -2,20 +2,39 @@
 
 if(!empty($_GET["login"] && $_GET["password"])){
 
-    // echo "login: " . $_GET["login"] . " hasło: " . $_GET["password"] . "  ";
+    $con = new mysqli("localhost", "root", "");
+    $con -> query("use chodzeniethegra");
 
-    if(strlen($_GET["login"]) < 5){
-        echo "za krótki login";
-    }else if(strlen($_GET["password"]) < 5){
-        echo "za krótke hasło";
-    }else{      
-            
+    $req = "select username, password from users";
+    $result = $con -> query($req); 
+    if($result->num_rows > 0){
+        while($row = $result->fetch_assoc()) {
+            $qol = true;
+            if($_GET["login"] == $row["username"]){
+                if($_GET["password"] == $row["password"]){
+                    echo "Logged in!";
+                    $_SESSION["username"] = $_GET['login'];
+                    $_SESSION["password"] = $_GET['password'];
+                    break;
+                }else{
+                    echo "Wrong password!";
+                }
+                $qol = false;
+            }        
+        }
 
+        if($qol){
+            echo "This loggin does not exist!";
+        }
+
+    }else{
+        echo "!err no results";
     }
-
+    
+    $con -> close();
 
 }else{
-    echo "nie git";
+    echo "Ale weź wprowadź dane :|";
 
 }
 
