84cba2dc8dcb020567a67a8663d83bf790997a56
Marekse <m.trzeciak.p20@gmail.com>
2022-11-28 20:01:08 +0100

    renamed

--- structure.sql
+++ structure.sql
@@ -1,6 +1,6 @@
-use ChodzenieTheGra;
+use chodzeniethegra;
 
-create table Users(
+create table users(
     id int auto_increment primary key,
     username varchar(30) not null,
     password varchar(30) not null,
