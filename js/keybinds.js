33fcd0c89a3d346aad5c82b18ab7a6f06fc9b3f6
Marekse <m.trzeciak.p20@gmail.com>
2022-12-03 17:11:54 +0100

    keybind work (adding deleting, display)

--- js/keybinds.js
+++ js/keybinds.js
@@ -1,45 +1,64 @@
 
 class KeybindsClass{
-
-    
+  
     constructor(){
 
-        if (KeybindsClass.exists) {
+        if(KeybindsClass.exists){
             return KeybindsClass.instance;
         }
 
-        this.forbidden = ["backspace", "tab", "enter", "shift", "crtl", "alt", "escape"]
+        this.forbidden = ["Backspace", "Tab", "Enter", "ShiftLeft", "ShiftRight", "ControlLeft", "AltRight", "AltLeft", "Escape"]
         this.binds_jump = ["KeyW", "ArrowUp"]
         this.binds_duck = ["KeyS", "ArrowDown"]
 
     }
 
-
-    DeleteInput(tab, input){
-        let index = tab.indexOf(input)
+    DeleteJumpInput(input){
+        input.length == 1 ? input = "Key" + input : true
+        let index = this.binds_jump.indexOf(input) + 1
         if(index == -1){
+            console.log(this.binds_jump, input, "not in array");
             return;
         }
-        tab.splice(index, 1)
-        if(this.binds_duck == tab){
-            console.log("ae")
-        }
-
+        this.binds_jump = this.binds_jump.splice(index, 1)
     }
 
-    AddInput(tab, input){
-        if(tab.indexOf(input) != -1 && !this.forbidden.includes(input) ){
+    DeleteDuckInput(input){
+        input.length == 1 ? input = "Key" + input : true
+        let index = this.binds_duck.indexOf(input) + 1
+        if(index == -1){
+
             return;
         }
-        tab.push(input)
-        console.log(window.removeEventListener("keydown", Event))
+        this.binds_duck = this.binds_duck.splice(index, 1)
+    }
+
+    AddJumpInput(input){
+        if( this.binds_jump.indexOf(input) != -1 || this.forbidden.includes(input) || this.binds_duck.indexOf(input) != -1 ){
+            return;
+        }
+        this.binds_jump.push(input)
+        window.removeEventListener("keydown", Event)
         this.selector.innerHTML = ""
         this.CreateKeybindsAdjustments(this.where)
     }
 
-    WaitForInput(tab){
-        window.addEventListener("keydown", (event) => this.AddInput(tab, event.code))
+    AddDuckInput(input){
+        if( this.binds_duck.indexOf(input) != -1 || this.forbidden.includes(input) || this.binds_jump.indexOf(input) != -1 ){
+            return;
+        }
+        this.binds_duck.push(input)
+        window.removeEventListener("keydown", Event)
+        this.selector.innerHTML = ""
+        this.CreateKeybindsAdjustments(this.where)
+    }
 
+    WaitForInput(name){
+        if(name == "jump"){
+            window.addEventListener("keydown", (event) => this.AddJumpInput(event.code))
+        }else{
+            window.addEventListener("keydown", (event) => this.AddDuckInput(event.code))
+        }
     }
 
     CreateKeybindsAdjustments(where){
@@ -50,13 +69,13 @@ class KeybindsClass{
         header.innerText = "keybinds"
         this.selector.appendChild(header)
 
-        this.selector.appendChild(this.CreateTable(this.binds_jump, "jump"))
-        this.selector.appendChild(this.CreateTable(this.binds_duck, "duck"))
+        this.selector.appendChild(this.CreateTable("jump"))
+        this.selector.appendChild(this.CreateTable("duck"))
 
         where.appendChild(this.selector)
     }
 
-    CreateTable(tab, name){
+    CreateTable(name){
 
         let column = document.createElement("DIV")
         column.setAttribute("class", "column")
@@ -66,13 +85,24 @@ class KeybindsClass{
         header.innerText = name
         column.appendChild(header)
 
-        for(let i in tab){
-            column.appendChild(this.CreateRow(tab[i].split("Key"), name))
-
+        
+        if(name == "jump"){
+            for(let i in this.binds_jump){
+                let value = this.binds_jump[i].split("Key")
+                value.length > 1 ? value.shift() : true
+                column.appendChild(this.CreateRow(value, name))               
+            }                   
+        }else{
+            for(let i in this.binds_duck){
+                let value = this.binds_duck[i].split("Key")
+                value.length > 1 ? value.shift() : true
+                column.appendChild(this.CreateRow(value, name))                
+            }   
         }
+
         this.add_input = document.createElement("div")
         this.add_input.setAttribute("class", "row")
-        this.add_input.addEventListener("click", () => {this.WaitForInput(tab)})
+        this.add_input.addEventListener("click", () => {this.WaitForInput(name)})
         this.add_input.innerText = "add input"
 
         column.appendChild(this.add_input)
@@ -80,7 +110,7 @@ class KeybindsClass{
         return column;   
     }
 
-    CreateRow(value, name){       //tworzy row z inputem
+    CreateRow(value, name){       //tworzy rząd z inputem
         let row = document.createElement("DIV")
         row.setAttribute("find", value)
         row.setAttribute("class", "row")
@@ -90,12 +120,16 @@ class KeybindsClass{
         return row;
     }
 
-    DeleteRow(value, name){       //usuwa dany row
+    DeleteRow(value, name){       //usuwa dany rząd
 
-        console.log(name, value,  "div[find='"+ name +"'] div[find='"+ value +"']")
+        // console.log(name, value,  "div[find='"+ name +"'] div[find='"+ value +"']")
         document.querySelector("#popup .keybinds_selector div[find='"+ name +"'] div[find='"+ value +"']").remove()
 
-        this.DeleteInput(value)
+        if(name == "jump"){
+            this.DeleteJumpInput(String(value))
+        }else{
+            this.DeleteDuckInput(String(value))
+        }
         return;
     }
 
